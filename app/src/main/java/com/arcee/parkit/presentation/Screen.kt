83ab695d5ade7e32e00a6ddc1992c1ea54742fa5
Shashika Jayawardhana <shashikaj@zone24x7.com>
2024-12-11 14:58:07 +0530

    Define new routes.

--- app/src/main/java/com/arcee/parkit/presentation/Screen.kt
+++ app/src/main/java/com/arcee/parkit/presentation/Screen.kt
@@ -1,8 +1,13 @@
 package com.arcee.parkit.presentation
 
 sealed class Screen(val route: String) {
+    object MainScreen : Screen("/")
     object HomeScreen : Screen("home")
     object ActivitiesScreen : Screen("activities")
     object ProfileScreen : Screen("profile")
     object NotificationsScreen : Screen("notifications")
+    object ProviderDetailScreen : Screen("provider")
+    object ActivityDetailScreen: Screen("activity-detail")
+    object ParkingSpaceLocatorScreen: Screen("parking-space-locator")
+    object SignInScreen: Screen("sign-in")
 }
