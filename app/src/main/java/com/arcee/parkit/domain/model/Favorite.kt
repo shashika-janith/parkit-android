a406e98b8b7f82375745a5e379af1a471c61845d
Shashika Jayawardhana <shashika.janith@gmail.com>
2025-04-11 09:51:57 +0530

    Define 'Favorite' model.

---
+++ app/src/main/java/com/arcee/parkit/domain/model/Favorite.kt
@@ -0,0 +1,13 @@
+package com.arcee.parkit.domain.model
+
+data class Favorite(
+    val id: Long,
+    val entityId: Long,
+    val name: String,
+    val address: String,
+    val latitude: Double,
+    val longitude: Double,
+    val capacity: Int,
+    val hourlyRate: Float,
+    val phone: String?,
+)
